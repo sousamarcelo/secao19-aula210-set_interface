5124dcf77e1deb89630c29abc9197e14235b7e98
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-24 18:49:34 -0300

    Buscando um produto da lista Set + listando todos os produtas ontidos no Set

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,24 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Program {
 	
 	public static void main(String[] args) {
+		
+		Set<String> set = new HashSet<String>();
+		
+		set.add("TV");
+		set.add("NoteBook");
+		set.add("Tablet");
+		
+		System.out.println("Na lista contem NoteBook: " + set.contains("NoteBook"));
+		System.out.println();
+		System.out.println("Lista de produtos inseridos no Set: ");
+		for(String s : set) {
+			System.out.println(s);
+		}
 		
 	}
 
